b4efc24405b6742c020858001eeede8749683de8
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-07-12 21:35:49 +0200

    features, testimonials section added

--- manage-landing-page-master(1)/manage-landing-page-master/app.js
+++ manage-landing-page-master(1)/manage-landing-page-master/app.js
@@ -9,8 +9,12 @@ const navSlide = () => {
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
+                document.getElementById('top').style.opacity = 1;
+
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + 0.3}s`;
+                document.getElementById('top').style.opacity = 0.1;
+
             }
         });
 
